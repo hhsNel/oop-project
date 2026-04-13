a010311dddaf0c3b7f78da5598e92d8bb6e1881d
MitrisStudios <michal.sikora0311@gmail.com>
2026-04-14 01:24:22 +0200

    aktor teraz korzysta z hp systems, nie pytaj o godzine

--- src/engine/actor.h
+++ src/engine/actor.h
@@ -2,16 +2,41 @@
 #define ACTOR_H
 
 #include "entity.h"
+#include "math/fxpt.h"
+#include "systems/systems.h"
+
 
 namespace engine {
 	class actor : virtual public entity {
-		unsigned int hp, shield;
+	protected:
+		systems::health_system health;
 	public:
-		actor(unsigned int const hp = 0, unsigned int const shield = 0);
+		
+		actor(math::fxpt hp, math::fxpt shield)
+		{
+			health.max_hp = hp;
+			health.current_hp = hp;
+			health.armor = shield;
+		}
 
-		virtual void take_damage(unsigned int const dmg);
-		virtual void heal(unsigned int const amount);
-		virtual void add_shield(unsigned int const amount);
+		virtual void take_damage(math::fxpt const dmg)
+		{
+			health.apply_damage(dmg, combat::status_type::none);
+		}
+		virtual void heal(math::fxpt const amount)
+		{
+			health.apply_heal(amount);
+		}
+		virtual void add_shield(math::fxpt const amount)
+		{
+			health.apply_shield(amount);
+		}
+
+		virtual void update(math::fxpt dt)
+		{
+			health.process_effects(dt);
+		}
+
 	};
 }
 
