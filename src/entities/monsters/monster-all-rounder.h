91d39a156eca0f232fe59c7f90bccffe9e4d2792
Michal <michal.sikora0311@gmail.com>
2026-05-23 18:00:50 +0200

    fix:monster hermetisation

--- src/entities/monsters/monster-all-rounder.h
+++ src/entities/monsters/monster-all-rounder.h
@@ -4,15 +4,13 @@
 
 namespace entities {
 
-	class monster_all_rounder : public monster, public util::componentized<monster_all_rounder> {
-		[[=util::component_field{}]] bool  melee_mode;
+	class monster_all_rounder : public monster {
+	protected:
+		bool  melee_mode;
+	private:
 		float melee_threshold;
 
-		friend class util::componentized<monster_all_rounder>;
 	public:
-		using util::componentized<monster_all_rounder>::operator();
-		using util::componentized<monster>::operator();
-
 		monster_all_rounder() : monster(80.0f, 20.0f, 2.0f, 5.0f, 10.0f, 10.0f, 1.0f),
 		    melee_mode(false), melee_threshold(2.5f) {}
 		void update(float dt) override;
--- src/entities/monsters/monster-sniper.h
+++ src/entities/monsters/monster-sniper.h
@@ -4,15 +4,12 @@
 
 namespace entities {
 
-	class monster_sniper : public monster, public util::componentized<monster_sniper> {
-		[[=util::component_field{}]] float shoot_interval;
-		[[=util::component_field{}]] float aim_timer;
+	class monster_sniper : public monster {
+	protected:
+		float shoot_interval;
+		float aim_timer;
 
-		friend class util::componentized<monster_sniper>;
 	public:
-		using util::componentized<monster_sniper>::operator();
-		using util::componentized<monster>::operator();
-
 		monster_sniper() : monster(40.0f, 0.0f, 1.0f, 25.0f, 30.0f, 30.0f, 3.0f),
 		    shoot_interval(3.0f), aim_timer(0.0f) {}
 		void update(float dt) override;
